1aa3ecefea271168f5e0da93fd7cfb56d55a1ff3
Marian <hellomariandev@gmail.com>
2024-01-20 17:53:11 +0100

    Update page.tsx

--- frontend/app/products/[slug]/page.tsx
+++ frontend/app/products/[slug]/page.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import Image from "next/image";
+import { shimmer, toBase64 } from "@/lib/image";
+import ProductInfo from "@/components/product-info";
 
-export default function Page() {
+async function getProduct(slug: string) {
+  const res = await fetch(
+    `http://127.0.0.1:1337/api/products?filters[slug]=${slug}&populate[image][populate]&populate[publisher][populate]=true`,
+    {
+      next: { revalidate: 0 },
+    }
+  );
+  return res.json();
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const product = await getProduct(params.slug);
+
   return (
-    <div>Product Page</div>
-  )
+    <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:max-w-none">
+        <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12 grid-rows-subgrid">
+          <div>
+            <Image
+              placeholder="blur"
+              blurDataURL={`data:image/svg+xml+webp;base64,${toBase64(
+                shimmer(225, 280)
+              )}`}
+              src={
+                "http://127.0.0.1:1337" +
+                product.data[0].attributes?.image.data.attributes.url
+              }
+              alt="Picture of the product"
+              width={255}
+              height={280}
+              sizes="(max-width: 768px) 100vw, 33vw"
+              className="h-full w-full object-cover hover:scale-105 duration-300"
+            />
+          </div>
+          <ProductInfo product={product.data[0].attributes} />
+          <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0"></div>
+        </div>
+      </div>
+    </main>
+  );
 }
