099f25938155e015234af718a402c3f8d903b274
Marian <hellomariandev@gmail.com>
2024-01-23 16:40:16 +0100

    Update site-header.tsx

--- frontend/components/site-header.tsx
+++ frontend/components/site-header.tsx
@@ -1,17 +1,44 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ShoppingBag } from "lucide-react";
+
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { ShoppingBag } from "lucide-react";
-import ThemeToggle from "./theme-toggle";
 import MainNav from "./main-nav";
+import ThemeToggle from "./theme-toggle";
 
 export default function SiteHeader() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const defaultSearchQuery = searchParams.get("search") ?? "";
+
+  function onSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const searchQuery = formData.get("search");
+    router.replace(`/?search=${searchQuery}`);
+  }
+
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
-          <Input placeholder="Search books..." className="h-9 lg:w-[300px]" />
+        <form
+          className="hidden items-center lg:inline-flex"
+          onSubmit={onSubmit}
+        >
+          <Input
+            id="search"
+            name="search"
+            type="search"
+            autoComplete="off"
+            placeholder="Search products..."
+            className="h-9 lg:w-[300px]"
+            defaultValue={defaultSearchQuery}
+          />
         </form>
 
         <div className="flex items-center space-x-1">
