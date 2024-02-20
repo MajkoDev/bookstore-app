653c6f3953ec952e41a5496541dafabd242ec90c
Stupel <hellomariandev@gmail.com>
2024-02-20 23:29:09 +0100

    frontend reconstructed

--- frontend/components/site-footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function SiteFooter() {
-  return (
-    <footer className="border-t">
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-12 sm:py-20 lg:px-8"></div>
-    </footer>
-  );
-}
