bb35f737cf126b5129f50c8d276e442c8f31bc86
Stupel <hellomariandev@gmail.com>
2024-02-21 00:04:15 +0100

    context

--- frontend/src/components/site-layout.jsx
+++ frontend/src/components/site-layout.jsx
@@ -6,15 +6,18 @@ import { Outlet } from "react-router";
 // Components
 import Footer from "./site-footer";
 import Header from "./site-header";
+import AppContext from "@/context";
 
 export default function RootLayout() {
   return (
     <div className="min-h-screen w-full flex flex-col">
-      <Header />
-      <main className="p-12 flex-1">
-        <Outlet />
-      </main>
-      <Footer />
+      <AppContext>
+        <Header />
+        <main className="p-12 flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </AppContext>
     </div>
   );
 }
