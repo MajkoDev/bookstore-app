d25e65044916c99b0b66c73dc048f375263312af
Stupel <hellomariandev@gmail.com>
2024-02-21 00:04:25 +0100

    header

--- frontend/src/components/site-header.jsx
+++ frontend/src/components/site-header.jsx
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
-    <div>
-      <h2 className="text-center text-xl font-light">Header</h2>
-    </div>
+    <header className="w-full py-2 px-6 md:px-12 lg:px-20">
+      <div className="flex flex-row justify-between items-center gap-2">
+        <h2 className="text-center text-xl font-light">Header</h2>
+        <div className="flex flex-row gap-2">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/cart">Cart</NavLink>
+        </div>
+      </div>
+    </header>
   );
 }
