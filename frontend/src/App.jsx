0fdc1f8a0eb25e2d8e25582e4df2153c81478324
Stupel <hellomariandev@gmail.com>
2024-02-20 23:57:24 +0100

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,35 @@
+// Router
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+// Layour
+import RootLayout from "./components/site-layout";
+
+// Pages
+import HomePage from "./pages/Home";
+import CartPage from "./pages/Cart";
+import CategoryPage from "./pages/Category";
+import ProductPage from "./pages/Product";
+import SuccessPage from "./pages/Success";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/category/:id" element={<CategoryPage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/success" element={<SuccessPage />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-medium text-center">Bookstore App</h1>
- 
-    </>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
