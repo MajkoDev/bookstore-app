8d080db8f4c40d0dc6c5661cb3774de6f43328f5
Stupel <hellomariandev@gmail.com>
2024-02-22 11:44:20 +0100

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -3,9 +3,14 @@ import { Context } from "@/context";
 import { makePaymentRequest } from "@/lib/api";
 import { loadStripe } from "@stripe/stripe-js";
 import CartItems from "@/components/cart-items";
+import { Card, CardContent } from "@/components/ui/card";
 
 function CartPage() {
   const { cartItems, cartSubTotal } = useContext(Context);
+
+  function isVariableValid(variable) {
+    return variable !== null && variable !== undefined;
+  }
 
   const stripePromise = loadStripe(
     "pk_test_51Ok9HJF1IXYkC9JQzInVqi4B5eIgwIP2tqyeF6rBPLjzIqlQ1yJJ7t8DCMpLwJrNqI0xAl3SpLVBWZ331kKJcxdx00HNeLQlcO"
@@ -13,7 +18,7 @@ function CartPage() {
 
   const handlePayment = async () => {
     try {
-      const stripe = await stripePromise
+      const stripe = await stripePromise;
       const res = await makePaymentRequest.post("/api/orders", {
         products: cartItems,
       });
@@ -26,15 +31,29 @@ function CartPage() {
     }
   };
 
-  return (
-    <div className="p-24">
-      <div className="text-center text-3xl font-medium">Shopping Cart</div>
-      
-      {!cartItems.length && (
-        <div>
-          <h1>Your shopping cart is empty.</h1>
+  if (isVariableValid(cartItems.length) && cartItems.length?.length === 0) {
+    return (
+      <div className="mb-4 grid grid-cols-1 md:grid-cols-3 gap-3">
+        <div className="md:col-span-2">
+          <Card>
+            <CardContent className="p-4">
+              <p>Your Cart is empty...</p>
+            </CardContent>
+          </Card>
         </div>
-      )}
+        <Receipt />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="my-4">
+        <h2 className="text-3xl font-bold tracking-tight">Shopping Cart</h2>
+        <p className="text-sm text-muted-foreground">
+          Below is a list of products you have in your cart.
+        </p>
+      </div>
 
       {!!cartItems.length && (
         <div>
@@ -46,7 +65,7 @@ function CartPage() {
           </button>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
