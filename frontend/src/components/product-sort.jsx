926858aefcc59a2b4a373e49807aff4cf5b88853
Stupel <hellomariandev@gmail.com>
2024-02-26 12:40:56 +0100

    product sort component

---
+++ frontend/src/components/product-sort.jsx
@@ -0,0 +1,55 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Filter } from "lucide-react";
+
+const sortOptions = [
+  { name: "Price, low to high", value: "asc" },
+  { name: "Price, high to low", value: "desc" },
+];
+
+function ProductSort({ setSort }) {
+  return (
+    <div className="flex items-center">
+      <Select onValueChange={(value) => setSort(value)}>
+        <SelectTrigger className="sm:w-[180px]">
+          <SelectValue placeholder="Sort By" />
+        </SelectTrigger>
+        <SelectContent>
+          {sortOptions.map((option) => (
+            <SelectItem key={option.name} value={option.value}>
+              {option.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Sheet>
+        <SheetContent className="w-[300px]">
+          <SheetHeader>
+            <SheetTitle>Categories</SheetTitle>
+            <SheetDescription>
+              Narrow your product search using the options below.
+            </SheetDescription>
+          </SheetHeader>
+          <ProductFilters />
+        </SheetContent>
+        <SheetTrigger className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden">
+          <span className="sr-only">Filters</span>
+          <Filter className="h-5 w-5" aria-hidden="true" />
+        </SheetTrigger>
+      </Sheet>
+    </div>
+  );
+}
