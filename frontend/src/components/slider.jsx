d4d25f2c1a2a39d8a4b004e9fb10b1428b846f3c
Stupel <hellomariandev@gmail.com>
2024-02-27 17:52:43 +0100

    slider

--- frontend/src/components/slider.jsx
+++ frontend/src/components/slider.jsx
@@ -3,37 +3,65 @@ import useFetchv2 from "@/hooks/useFetchv2";
 import { ArrowLeftCircle, ArrowRightCircle } from "lucide-react";
 
 export default function Slider() {
-  const [curIndex, setCurIndex] = useState(0);
+  const { data } = useFetchv2(`http://localhost:1337/api/slider?populate=*`);
+  let slides = data?.attributes?.images?.data;
 
-  const { data: slider } = useFetchv2(
-    `http://localhost:1337/api/slider?populate=*`
-  );
+  const [current, setCurrent] = useState(0);
 
-  const prevSlide = () => {
-  const newIndex = (curIndex - 1 + slider.length) % slider.length;
-
-    setCurIndex(newIndex);
+  let previousSlide = () => {
+    if (current === 0) setCurrent(slides.length - 1);
+    else setCurrent(current - 1);
   };
-  const nextSlide = () => {
-    const newIndex = (curIndex + 1) % slider.length;
 
-    setCurIndex(newIndex);
+  let nextSlide = () => {
+    if (current === slides.length - 1) setCurrent(0);
+    else setCurrent(current + 1);
   };
+  
 
   return (
-    <div className="h-96 w-full rounded-md relative group">
-      <div
-        style={{
-          backgroundImage: `url("http://127.0.0.1:1337${slider?.attributes?.images?.data[curIndex].attributes.url}")`,
-        }}
-        className="w-full h-full bg-center bg-cover duration-500 opacity-95"
-      ></div>
-      <div className="hidden group-hover:block absolute top-[50%] -translate-y-[50%] left-5 text-2xl rounded-full bg-black/20 text-white cursor-pointer">
-        <ArrowLeftCircle className="w-6 h-6" onClick={prevSlide} />
-      </div>
-      <div className="hidden group-hover:block absolute top-[50%] -translate-y-[50%] right-5 text-2xl rounded-full bg-black/20 text-white cursor-pointer">
-        <ArrowRightCircle className="w-6 h-6" onClick={nextSlide} />
-      </div>
+    <div className="overflow-hidden flex justify-center items-center relative h-96 rounded-md">
+    <div
+      className={`flex transition ease-in-out duration-300`}
+      style={{
+        transform: `translateX(-${current * 100}%)`,
+      }}
+    >
+      {slides?.map((s) => {
+        return (
+          <img
+            key={s.id}
+            src={`http://127.0.0.1:1337${s.attributes.url}`}
+            className="object-cover object-center h-full w-full"
+            />
+        );
+      })}
     </div>
+
+    <div className="absolute top-0 h-full w-full justify-between items-center flex text-white px-10 text-3xl">
+      <button onClick={previousSlide}>
+        <ArrowLeftCircle />
+      </button>
+      <button onClick={nextSlide}>
+        <ArrowRightCircle />
+      </button>
+    </div>
+
+    <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
+      {slides?.map((s, i) => {
+        return (
+          <div
+            onClick={() => {
+              setCurrent(i);
+            }}
+            key={"circle" + i}
+            className={`rounded-full w-3 h-3 cursor-pointer  ${
+              i == current ? "bg-white" : "bg-slate-300"
+            }`}
+          ></div>
+        );
+      })}
+    </div>
+  </div>
   );
 }
